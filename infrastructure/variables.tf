fcaf3dcee6b80920d8e3058ceba5bbc4e2918898
gavan <gavan.jon.lamb@gmail.com>
2021-08-01 19:29:22 +1000

    Update tags

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -20,7 +20,8 @@ locals {
   domain = "auth.${var.base_domain_name}"
   default_tags = {
     Application = "Expensely"
-    Team = "Expensely"
+    Team = "Expensely Core"
     ManagedBy = "Terraform"
+    Environment = var.environment
   }
 }
