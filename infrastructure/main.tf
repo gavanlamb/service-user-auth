868fc779a21453749abd5c98fdd29f08b126a5c5
gavan <gavan.jon.lamb@gmail.com>
2021-08-03 23:23:05 +1000

    Update acm domain

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -99,7 +99,7 @@ resource "aws_route53_record" "auth" {
 data "aws_acm_certificate" "wildcard" {
   provider = aws.us-east-1
 
-  domain = "*.${var.base_domain_name}"
+  domain = var.base_domain_name
   statuses = ["ISSUED"]
 }
 
