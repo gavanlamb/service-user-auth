3a196e3ea04ffd79258baef9394eccd4a8e0aeb8
gavan <gavan.jon.lamb@gmail.com>
2021-11-07 17:27:41 +1100

    Move clients and resource servers

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -1,8 +1,8 @@
 data "aws_route53_zone" "expensely" {
-  name = var.cognito_client_app_domain
+  name = var.app_domain
 }
 data "aws_route53_zone" "zone" {
-  name = "${var.cognito_client_app_domain}."
+  name = "${var.app_domain}."
 }
 
 data "aws_caller_identity" "current" {}
